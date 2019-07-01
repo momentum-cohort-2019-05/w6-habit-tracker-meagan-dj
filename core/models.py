96fd41b9596248461b71857c84138067d586ca47
Meagan-M <meaganmakinson80@gmail.com>
2019-07-01 13:38:42 -0400

    add relationships

--- core/models.py
+++ core/models.py
@@ -6,17 +6,22 @@ class Habit(models.Model):
     name = models.Charfield(max_length=300, help_text= "Enter your habit here")
     description = models.TextField(help_text="Enter the decription of your habit here")
     target = models.IntegerField(help_text = "Enter a target number for your habit")
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    observers = models.ManyToManyField(Observer)
 
 class DailyRecord(models.Model):
     day_number = models.IntegerField(help_text = "Enter the day number for your habit")
     date = models.DateField(help_text = "Enter the date")
     description = models.TextField(help_text="Enter the description of your progress here")
     progress = models.IntegerField(help_text = "Enter your progress here")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
     description = models.TextField(help_text="Enter your comment here")
+    observer = models.ForeignKey(Observer, on_delete=models.CASCADE)
+    daily_record = models.ForeignKey(DailyRecord, on_delete=models.CASCADE)
 
 
 class Observer(models.Model):
